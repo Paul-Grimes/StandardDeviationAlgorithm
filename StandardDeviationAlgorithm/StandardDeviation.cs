f436927d458178be0e5bd6c4280bf988a874b073
Paul Grimes <paul.grimes@byprime.com>
2013-07-16 17:41:30 -0400

    Get the square root of the variance

---
+++ StandardDeviationAlgorithm/StandardDeviation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardDeviationAlgorithm
+{
+    class StandardDeviation
+    {
+        public static double SimpleStandardDeviation(double[] values)
+        {
+            return Math.Sqrt(VarianceAlgorithm.Variance.simple_variance(values));
+        }
+    }
+}
