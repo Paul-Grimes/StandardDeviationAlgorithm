bee65fbd0c869567f02fcac2750c7dea96b72807
Paul Grimes <paul.grimes@byprime.com>
2013-07-16 17:41:01 -0400

    Execute the standard deviation

--- StandardDeviationAlgorithm/Program.cs
+++ StandardDeviationAlgorithm/Program.cs
@@ -10,6 +10,10 @@ namespace StandardDeviationAlgorithm
     {
         static void Main(string[] args)
         {
+            double[] myArray = { 34.7, 23.6, 28.5, 37.8, 35.4, 28.6, 37.3, 48.9, 24.0 };
+
+            Console.WriteLine(StandardDeviation.SimpleStandardDeviation(myArray));
+
         }
     }
 }
