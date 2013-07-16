04b07d4206d03e43bdcfb8452286e6b9abac58ce
Paul Grimes <paul.grimes@byprime.com>
2013-07-16 17:12:09 -0400

    initial commit wiht copy of Variance.cs added

---
+++ StandardDeviationAlgorithm/Variance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarianceAlgorithm
+{
+    class Variance
+    {
+        public static int simple_variance(int[] values)
+        {
+            float total = 0;
+            float totaledSquared = 0;
+            float counter = 0;
+           
+
+            foreach (int value in values)
+            {
+                counter ++;
+                total += value;
+                totaledSquared += value * value;
+            }
+
+            return Convert.ToInt32((totaledSquared - ((total * total) / counter)) / (counter - 1));
+        }
+    }
+}
